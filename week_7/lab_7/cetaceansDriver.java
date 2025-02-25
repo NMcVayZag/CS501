1e8dc5e359fbb7b284cb5b1ace33e0ad2be04d32
NMcVayZag <Nickwmcvay15@gmail.com>
2025-02-25 11:08:26 -0800

    update

--- week_7/lab_7/cetaceansDriver.java
+++ week_7/lab_7/cetaceansDriver.java
@@ -2,6 +2,9 @@ package lab_7;
 
 public class cetaceansDriver {
     public static void main(String[] args) {
+        System.out.println("\nHere is a brief breakdown of the aquatic Cetacean famliy species with two descendant families and a species from each of those descendant families\n");
+
+        
         Cetacean animal;
         animal = new Cetacean();
         System.out.println("Animal variable is currently referencing the Cetacean which is a "+ animal.getDesignation()+"\n");
@@ -22,11 +25,6 @@ public class cetaceansDriver {
         animal = new BlueWhale();
         System.out.println("Animal variable is currently referencing the Mysticeti which is a "+ animal.getDesignation()+"\n");
         animal.getProperties();
-
-
-    
-        
-
 
         
     }
